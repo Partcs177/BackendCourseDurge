2aa35ba9be0885aa136d1f3f44b14414c942ad3a
Partcs177 <parshu.tcslearning@gmail.com>
2022-04-27 15:51:07 +0530

    Constant Class added

--- src/main/java/com/codewithdurgesh/blog/controllers/PostController.java
+++ src/main/java/com/codewithdurgesh/blog/controllers/PostController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codewithdurgesh.blog.config.AppConstants;
 import com.codewithdurgesh.blog.entities.Post;
 import com.codewithdurgesh.blog.payloads.ApiResponse;
 import com.codewithdurgesh.blog.payloads.PostDto;
@@ -53,10 +54,10 @@ public class PostController {
 	//get all posts
 	@GetMapping("/posts")
 	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value="pageNumber",defaultValue="0",required=false) Integer pageNumber,
-			@RequestParam(value="pageSize",defaultValue="10",required=false) Integer pageSize,
-			@RequestParam(value="sortBy",defaultValue="postId",required=false) String sortBy,
-			@RequestParam(value="sortDir",defaultValue="asc",required=false) String sortDir
+			@RequestParam(value="pageNumber",defaultValue=AppConstants.PAGE_NUMBER,required=false) Integer pageNumber,
+			@RequestParam(value="pageSize",defaultValue=AppConstants.PAGE_SIZE,required=false) Integer pageSize,
+			@RequestParam(value="sortBy",defaultValue=AppConstants.SORT_BY,required=false) String sortBy,
+			@RequestParam(value="sortDir",defaultValue=AppConstants.SORT_DIR,required=false) String sortDir
 			){
 		
 		return new ResponseEntity<PostResponse>(this.postservice.getAllPost(pageNumber, pageSize,sortBy,sortDir), HttpStatus.OK);
